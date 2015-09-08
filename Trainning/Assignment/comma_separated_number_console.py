89dae7ccb37ee18814241860af2bcd4732cabdc0
Pratibh <19219prtv@gmail.com>
2015-09-08 11:12:08 +0545

    Files after third workshop

--- Trainning/Assignment/comma_separated_number_console.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'pratibh'
-
-print "Enter comma separated numbers"
-data=raw_input()
-
-list_to_print=data.split(",")
-tuple_to_print=tuple(list_to_print)
-print list_to_print
-print tuple_to_print
